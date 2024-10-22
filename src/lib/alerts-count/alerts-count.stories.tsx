5a7de0e73bc2b0ef6f46cc3bebdd07cd51f7650c
Максим Коноваленко <ii4elka@MacBook-Pro-Maksim.local>
2024-10-22 09:54:04 +0300

    feat: add icons for device creation

--- src/lib/alerts-count/alerts-count.stories.tsx
+++ src/lib/alerts-count/alerts-count.stories.tsx
@@ -1,5 +1,16 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { AlertsCount } from '@chirp/ui/lib';
+import {
+    DeviceIcon,
+    EnergyIcon,
+    LoRaDeviceIcon,
+    LoRaDeviceIconBig,
+    SimCardIcon,
+    SimDeviceBigIcon,
+    WiFiIcon,
+} from '@chirp/ui/assets/icons';
+import { useTheme } from '@emotion/react';
+import { SimCardsIcon } from '@chirp/ui/assets/fleet-icons';
 
 const meta: Meta<typeof AlertsCount> = {
     title: 'UI/AlertsCount',
@@ -16,4 +27,32 @@ type Story = StoryObj<typeof AlertsCount>;
 
 export const Default: Story = {
     render: () => <AlertsCount>2</AlertsCount>,
+};
+
+export const Icons: Story = {
+    render: () => {
+        const theme = useTheme();
+        return <LoRaDeviceIcon theme={theme} />;
+    },
+};
+
+export const IconsBig: Story = {
+    render: () => {
+        const theme = useTheme();
+        return <LoRaDeviceIconBig theme={theme} />;
+    },
+};
+
+export const GPSIcon: Story = {
+    render: () => {
+        const theme = useTheme();
+        return <EnergyIcon color={'green'} />;
+    },
+};
+
+export const GPSIcon2: Story = {
+    render: () => {
+        const theme = useTheme();
+        return <SimDeviceBigIcon />;
+    },
 };
