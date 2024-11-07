c9556f91f1136d24733f24866ed9c80ab25905bc
scotch <igaraev@solit-clouds.ru>
2024-11-07 20:24:08 +0300

    feat: dev line drawing

--- src/lib/map/map.stories.tsx
+++ src/lib/map/map.stories.tsx
@@ -253,3 +253,21 @@ export const ClearData: Story = {
         );
     },
 };
+
+export const EmptyDrawable: Story = {
+    render: () => {
+        const [drawState, setDrawState] = useState<GeoJSON.GeoJSON | null>(null);
+
+        return (
+            <Box sx={{ width: '1200px', height: '1200px' }}>
+                <Map
+                    onChange={setDrawState}
+                    data={drawState}
+                    isSingleDraw
+                    coordinates={{ lon: 49.108891, lat: 55.796391 }}
+                    isDrawable
+                />
+            </Box>
+        );
+    },
+};
