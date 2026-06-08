58cc45cd7e001c91793361491c587a6436c16fcc
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2026-06-08 09:57:08 +0300

    [CHIRP-3246]: build - B3.2 expose TextFieldStyled

--- dist/primitives.es.js
+++ dist/primitives.es.js
@@ -1,14 +1,16 @@
 import { Button as t } from "./components/Button/Button.es.js";
-import { IconButton as p } from "./components/IconButton/index.es.js";
+import { IconButton as x } from "./components/IconButton/index.es.js";
 import { TextField as f } from "./components/TextField/TextField.es.js";
-import { MaskedInput as n } from "./components/TextField/MaskedInput.es.js";
-import { PhoneField as d } from "./components/PhoneField/PhoneField.es.js";
+import { MaskedInput as d } from "./components/TextField/MaskedInput.es.js";
+import { TextField as l } from "./components/TextField/style.es.js";
+import { PhoneField as u } from "./components/PhoneField/PhoneField.es.js";
 import { SearchInput as a } from "./components/SearchInput/index.es.js";
 export {
   t as Button,
-  p as IconButton,
-  n as MaskedInput,
-  d as PhoneField,
+  x as IconButton,
+  d as MaskedInput,
+  u as PhoneField,
   a as SearchInput,
-  f as TextField
+  f as TextField,
+  l as TextFieldStyled
 };
