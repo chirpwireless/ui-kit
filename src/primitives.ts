134f31eedf83c83af00c12806d5302b5d002e6dc
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2026-06-08 15:38:35 +0300

    [CHIRP-3246]: B3.9 - export DEFAULT_ESTIMATE_SIZE from primitives

--- src/primitives.ts
+++ src/primitives.ts
@@ -75,4 +75,6 @@ export { GeneralInfoCard } from './components/GeneralInfoCard';
 
 export { Table, TableVirtualized, TableVirtualizedInfinite } from './components/Table';
 
+export { DEFAULT_ESTIMATE_SIZE } from './components/Table/TableVirtualizedInfinite';
+
 export type { TableColumn, TableColumnDef, ExtendedTableColumnDef } from './components/Table';
