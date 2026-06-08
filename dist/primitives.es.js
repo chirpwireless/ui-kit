2e68b8700ac27849b9c01ea203525eb55d974206
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2026-06-08 15:38:03 +0300

    [CHIRP-3246]: build - B3.9 Table + DEFAULT_ESTIMATE_SIZE export

--- dist/primitives.es.js
+++ dist/primitives.es.js
@@ -3,85 +3,86 @@ import { IconButton as m } from "./components/IconButton/index.es.js";
 import { TextField as x } from "./components/TextField/TextField.es.js";
 import { MaskedInput as f } from "./components/TextField/MaskedInput.es.js";
 import { TextField as i } from "./components/TextField/style.es.js";
-import { PhoneField as c } from "./components/PhoneField/PhoneField.es.js";
-import { SearchInput as S } from "./components/SearchInput/index.es.js";
-import { Select as T } from "./components/Select/Select.es.js";
+import { PhoneField as S } from "./components/PhoneField/PhoneField.es.js";
+import { SearchInput as d } from "./components/SearchInput/index.es.js";
+import { Select as g } from "./components/Select/Select.es.js";
 import { InputLabel as C } from "./components/Select/components/InputLabel/index.es.js";
-import { Autocomplete as b } from "./components/Autocomplete/index.es.js";
-import { SelectIndicator as I } from "./components/SelectIndicator/index.es.js";
+import { Autocomplete as k } from "./components/Autocomplete/index.es.js";
+import { SelectIndicator as B } from "./components/SelectIndicator/index.es.js";
 import { Checkbox as F } from "./components/Checkbox/Checkbox.es.js";
-import { MobileToggle as s, Toggle as w } from "./components/Toggle/index.es.js";
-import { ThemeSwitch as R } from "./components/ThemeSwitch/ThemeSwitch.es.js";
-import { MultiSelectOrCreate as z } from "./components/MultiSelectOrCreate/MultiSelectOrCreate.es.js";
-import { Tabs as J } from "./components/Tabs/Tabs.es.js";
-import { TabBadge as V, TabsContainer as v } from "./components/Tabs/style.es.js";
-import { Tooltip as G } from "./components/Tooltip/Tooltip.es.js";
-import { Badge as P, SimpleBadge as j } from "./components/Badge/index.es.js";
-import { FilterItem as H } from "./components/FilterItem/index.es.js";
-import { StackColumn as N, StackColumnS as Q, StackRow as U, StackRowJB as W, StackRowJC as X } from "./components/Stacks/index.es.js";
-import { CloseButton as Z, Dialog as _, DialogContent as $ } from "./components/Dialog/Dialog.es.js";
+import { MobileToggle as s, Toggle as E } from "./components/Toggle/index.es.js";
+import { ThemeSwitch as w } from "./components/ThemeSwitch/ThemeSwitch.es.js";
+import { MultiSelectOrCreate as L } from "./components/MultiSelectOrCreate/MultiSelectOrCreate.es.js";
+import { Tabs as y } from "./components/Tabs/Tabs.es.js";
+import { TabBadge as J, TabsContainer as V } from "./components/Tabs/style.es.js";
+import { Tooltip as v } from "./components/Tooltip/Tooltip.es.js";
+import { Badge as O, SimpleBadge as P } from "./components/Badge/index.es.js";
+import { FilterItem as Z } from "./components/FilterItem/index.es.js";
+import { StackColumn as q, StackColumnS as H, StackRow as K, StackRowJB as N, StackRowJC as Q } from "./components/Stacks/index.es.js";
+import { CloseButton as X, Dialog as Y, DialogContent as $ } from "./components/Dialog/Dialog.es.js";
 import { DeviceDialog as eo, DialogIconButton as ro } from "./components/Dialog/styles.es.js";
 import { Modal as mo } from "./components/Modal/index.es.js";
 import { ConfirmationDialog as xo } from "./components/ConfirmationDialog/ConfirmationDialog.es.js";
 import { DialogForm as fo } from "./components/DialogForm/DialogForm.es.js";
 import { ActionDialog as io } from "./components/ActionDialog/index.es.js";
-import { Loader as co } from "./components/Loader/index.es.js";
-import { Skeleton as go } from "./components/Skeleton/Skeleton.es.js";
+import { Loader as So } from "./components/Loader/index.es.js";
+import { Skeleton as To } from "./components/Skeleton/Skeleton.es.js";
 import { TextSkeleton as uo } from "./components/TextSkeleton/TextSkeleton.es.js";
-import { EmptyBlock as ko } from "./components/EmptyBlock/EmptyBlock.es.js";
-import { Card as Bo } from "./components/Card/index.es.js";
+import { EmptyBlock as Io } from "./components/EmptyBlock/EmptyBlock.es.js";
+import { Card as bo } from "./components/Card/index.es.js";
 import { SimpleCard as Do, SimpleSmallCard as Fo } from "./components/SimpleCard/index.es.js";
 import { GeneralInfoCard as so } from "./components/GeneralInfoCard/GeneralInfoCard.es.js";
 import { Table as Mo } from "./components/Table/Table.es.js";
-import { TableVirtualized as yo } from "./components/Table/TableVirtualized.es.js";
-import { TableVirtualizedInfinite as Ao } from "./components/Table/TableVirtualizedInfinite.es.js";
+import { TableVirtualized as Ao } from "./components/Table/TableVirtualized.es.js";
+import { DEFAULT_ESTIMATE_SIZE as Ro, TableVirtualizedInfinite as yo } from "./components/Table/TableVirtualizedInfinite.es.js";
 export {
   io as ActionDialog,
-  b as Autocomplete,
-  P as Badge,
+  k as Autocomplete,
+  O as Badge,
   r as Button,
-  Bo as Card,
+  bo as Card,
   F as Checkbox,
-  Z as CloseButton,
+  X as CloseButton,
   xo as ConfirmationDialog,
+  Ro as DEFAULT_ESTIMATE_SIZE,
   eo as DeviceDialog,
-  _ as Dialog,
+  Y as Dialog,
   $ as DialogContent,
   fo as DialogForm,
   ro as DialogIconButton,
-  ko as EmptyBlock,
-  H as FilterItem,
+  Io as EmptyBlock,
+  Z as FilterItem,
   so as GeneralInfoCard,
   m as IconButton,
   C as InputLabel,
-  co as Loader,
+  So as Loader,
   f as MaskedInput,
   s as MobileToggle,
   mo as Modal,
-  z as MultiSelectOrCreate,
-  c as PhoneField,
-  S as SearchInput,
-  T as Select,
-  I as SelectIndicator,
-  j as SimpleBadge,
+  L as MultiSelectOrCreate,
+  S as PhoneField,
+  d as SearchInput,
+  g as Select,
+  B as SelectIndicator,
+  P as SimpleBadge,
   Do as SimpleCard,
   Fo as SimpleSmallCard,
-  go as Skeleton,
-  N as StackColumn,
-  Q as StackColumnS,
-  U as StackRow,
-  W as StackRowJB,
-  X as StackRowJC,
-  V as TabBadge,
+  To as Skeleton,
+  q as StackColumn,
+  H as StackColumnS,
+  K as StackRow,
+  N as StackRowJB,
+  Q as StackRowJC,
+  J as TabBadge,
   Mo as Table,
-  yo as TableVirtualized,
-  Ao as TableVirtualizedInfinite,
-  J as Tabs,
-  v as TabsContainer,
+  Ao as TableVirtualized,
+  yo as TableVirtualizedInfinite,
+  y as Tabs,
+  V as TabsContainer,
   x as TextField,
   i as TextFieldStyled,
   uo as TextSkeleton,
-  R as ThemeSwitch,
-  w as Toggle,
-  G as Tooltip
+  w as ThemeSwitch,
+  E as Toggle,
+  v as Tooltip
 };
